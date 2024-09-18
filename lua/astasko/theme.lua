c9b36df5a014f1859df4c12deb4de39311e804e1
Aleksandr Stasko <aleksandr.stasko@burberry.com>
2024-09-19 00:25:45 +0300

    fix: notify background

--- lua/astasko/theme.lua
+++ lua/astasko/theme.lua
@@ -6,51 +6,52 @@ local notify = require 'notify'
 local catppuccin = require 'catppuccin'
 
 local function init()
-  colorizer.setup {}
+	colorizer.setup {}
 
-  comment.setup {}
+	comment.setup {}
 
-  gitsigns.setup {}
+	gitsigns.setup {}
 
-  notify.setup {
-    render = "wrapped-compact",
-    timeout = 2500,
-  }
+	notify.setup {
+		render = "wrapped-compact",
+		timeout = 2500,
+		background_colour = "#000000",
+	}
 
-  noice.setup {
-    lsp = {
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-      },
-    },
-    presets = {
-      bottom_search = true,
-      command_palette = true,
-      inc_rename = false,
-      long_message_to_split = true,
-      lsp_doc_border = false,
-    },
-  }
+	noice.setup {
+		lsp = {
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+			},
+		},
+		presets = {
+			bottom_search = true,
+			command_palette = true,
+			inc_rename = false,
+			long_message_to_split = true,
+			lsp_doc_border = false,
+		},
+	}
 
-  catppuccin.setup {
-    flavour = "macchiato",
-    integrations = {
-      cmp = true,
-      gitsigns = true,
-      native_lsp = {
-        enabled = true,
-      },
-      telescope = true,
-      treesitter = true,
-    },
-    term_colors = true,
-    transparent_background = true,
-  }
+	catppuccin.setup {
+		flavour = "macchiato",
+		integrations = {
+			cmp = true,
+			gitsigns = true,
+			native_lsp = {
+				enabled = true,
+			},
+			telescope = true,
+			treesitter = true,
+		},
+		term_colors = true,
+		transparent_background = true,
+	}
 
-  vim.cmd.colorscheme "catppuccin"
+	vim.cmd.colorscheme "catppuccin"
 end
 
 return {
-  init = init,
+	init = init,
 }
