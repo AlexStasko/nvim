4386ebfb37d2ed08f3a9e09ac4c13c50ffb54e5f
Aleksandr Stasko <aleksandr.stasko@burberry.com>
2024-12-01 23:38:00 +0400

    fix: change shiftwidth and tabstop

--- lua/astasko/vim.lua
+++ lua/astasko/vim.lua
@@ -38,8 +38,8 @@ local function init()
 
     vim.wo.wrap = false
 
-    vim.o.shiftwidth = 4
-    vim.o.tabstop = 4
+    vim.o.shiftwidth = 2
+    vim.o.tabstop = 2
     vim.o.expandtab = true
 end
 
